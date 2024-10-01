516e919c24968583516f6e8314cff7ef7cacb846
aravind452 <aravindaru452@gmail.com>
2024-10-01 15:06:22 +0530

    first commit

---
+++ firstwebappusingspringboot/src/main/java/com/example/firstwebappusingspringboot/FirstwebappusingspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstwebappusingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwebappusingspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstwebappusingspringbootApplication.class, args);
+	}
+
+}
---
+++ firstwebappusingspringboot/src/test/java/com/example/firstwebappusingspringboot/FirstwebappusingspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstwebappusingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstwebappusingspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
